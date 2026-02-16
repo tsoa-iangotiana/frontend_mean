1c3172bce9df1581102abd597ec786886cd42048
tsoa-iangotiana <152261956+tsoa-iangotiana@users.noreply.github.com>
2026-02-16 21:47:57 +0300

    need for deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = { 
   production: true, 
-  apiUrl: ' https://backend-mean-cpea.onrender.com'  // URL pour la production 
+  apiUrl: ' https://backend-mean-cpea.onrender.com'  // URL pour la production render
 }; 
 // https://backend-mean-cpea.onrender.com
